9f752fcd6ee15d300ef93587787476e869c1cd5c
aaronfern <aaron.francis.fernandes@sap.com>
2023-03-29 22:21:10 +0530

    Added cache for ready check and added unit test cases to all packages

--- internal/bootstrap/bootstrap_test.go
+++ internal/bootstrap/bootstrap_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/gardener/etcd-wrapper/internal/brclient"
+	. "github.com/onsi/gomega"
 )
 
 var (
@@ -14,110 +15,77 @@ var (
 	exitCodeFilePath = pwd + "/../../test/exit_code"
 )
 
-func TestCleanupExitCode(t *testing.T) {
-	// 1) When exit_code file exists
-	// Create exit_code file
+func TestCleanupExitCodeWhenFileExists(t *testing.T) {
+	g := NewWithT(t)
+
+	// create exit code file
 	interrupt := []byte("test")
 	err := os.WriteFile(exitCodeFilePath, interrupt, 0644)
-	if err != nil {
-		t.Errorf("Error creating exit_code file: %v", err)
-	}
+	g.Expect(err).To(BeNil())
 	// Call CleanupExitCode
 	err = CleanupExitCode(exitCodeFilePath)
-	if err != nil {
-		t.Errorf("Error cleaning exit_code file: %v", err)
-	}
+	g.Expect(err).To(BeNil())
 	// Check if exit_code file exists
 	_, err = os.Stat(exitCodeFilePath)
-	if err == nil {
-		t.Errorf("exit_code file still exists after CleanupExitCode() succeeds: %v", err)
-	}
+	g.Expect(err).ToNot(BeNil())
+}
 
-	// 2) When exit_code file does not exist
-	//Remove exit_code file if it exists
-	_, err = os.Stat(exitCodeFilePath)
+func TestCleanupExitCodeWhenFileDoesNotExists(t *testing.T) {
+	g := NewWithT(t)
+	// check is exit_code file exists
+	// remove it if it does
+	_, err := os.Stat(exitCodeFilePath)
 	if err != nil {
 		_ = os.Remove(exitCodeFilePath)
 	}
+	// call CleanupExitCode
 	err = CleanupExitCode(exitCodeFilePath)
-	if err != nil {
-		t.Error("CleanupExitCode should not throw an error")
-	}
+	g.Expect(err).To(BeNil())
 }
 
 func TestCaptureExitCode(t *testing.T) {
+	g := NewWithT(t)
 	//Test with os.Interrupt
 	CaptureExitCode(os.Interrupt, exitCodeFilePath)
+
 	//Check exit_code file
 	fileDataBytes, err := os.ReadFile(exitCodeFilePath)
-	if err != nil {
-		t.Errorf("Error reading exit_code file: %v", err)
-	}
-	fileData := strings.TrimSpace(string(fileDataBytes))
-	if fileData != os.Interrupt.String() {
-		t.Error("Wrong exit_code captured")
-	}
+	g.Expect(err).To(BeNil())
+	g.Expect(strings.TrimSpace(string(fileDataBytes))).To(Equal(os.Interrupt.String()))
 	//Delete exit_code file
 	err = os.Remove(exitCodeFilePath)
-	if err != nil {
-		t.Errorf("Error deleting exit_code file %v", err)
-	}
-
-	//Test with SIGTERM signal
-	CaptureExitCode(syscall.SIGTERM, exitCodeFilePath)
-	//Check exit_code file
-	fileDataBytes, err = os.ReadFile(exitCodeFilePath)
-	if err != nil {
-		t.Errorf("Error reading exit_code file: %v", err)
-	}
-	fileData = strings.TrimSpace(string(fileDataBytes))
-	if fileData != syscall.SIGTERM.String() {
-		t.Error("Wrong exit_code captured")
-	}
-	//Delete exit_code file
-	err = os.Remove(exitCodeFilePath)
-	if err != nil {
-		t.Errorf("Error deleting exit_code file %v", err)
-	}
+	g.Expect(err).To(BeNil())
 }
 
 func TestGetValidationMode(t *testing.T) {
-	tests := []struct {
-		name                   string
+	table := []struct {
 		description            string
 		exitCode               string
 		expectedValidationMode brclient.ValidationType
-		run                    func(t *testing.T, exitCode string, expectedValidationMode brclient.ValidationType)
 	}{
-		{"NoExitCode", "Exit code file does not exist", "", brclient.FullValidation, testValidationMode},
-		{"Interrupt", "Exit code has error string interrupt", os.Interrupt.String(), brclient.SanityValidation, testValidationMode},
-		{"Terminated", "Exit code has error string terminated", syscall.SIGTERM.String(), brclient.SanityValidation, testValidationMode},
-		{"Random", "Exit code has any other error string", "test", brclient.FullValidation, testValidationMode},
+		{"test: exit code file not being present should result in full validation", "", brclient.FullValidation},
+		{"test: exit code having error string `interrupt` should result in sanity validation", os.Interrupt.String(), brclient.SanityValidation},
+		{"test: exit code having error string `terminated` should result in sanity validation", syscall.SIGTERM.String(), brclient.SanityValidation},
+		{"test: exit code having any other error string should result in full validation", "test", brclient.FullValidation},
 	}
-	for _, test := range tests {
-		t.Run(test.description, func(t *testing.T) {
-			test.run(t, test.exitCode, test.expectedValidationMode)
-		})
-	}
-}
+	for _, entry := range table {
+		t.Run(entry.description, func(t *testing.T) {
+			t.Log(entry.description)
 
-func testValidationMode(t *testing.T, exitCode string, expectedValidationMode brclient.ValidationType) {
-	// Create exit_code file
-	if exitCode != "" {
-		err := os.WriteFile(exitCodeFilePath, []byte(exitCode), 0644)
-		if err != nil {
-			t.Errorf("Error writing to exit_code file: %v", err)
-		}
-	}
-	validationMode := getValidationMode(exitCodeFilePath)
-	if validationMode != expectedValidationMode {
-		t.Errorf("Wrong Validation mode. Expected %v, but got %v", expectedValidationMode, validationMode)
-	}
-	// Delete exit_code file
-	if exitCode != "" {
-		err := os.Remove(exitCodeFilePath)
-		if err != nil {
-			t.Errorf("Error removing exit_code file: %v", err)
-		}
+			g := NewWithT(t)
+
+			// Create exit_code file
+			if entry.exitCode != "" {
+				err := os.WriteFile(exitCodeFilePath, []byte(entry.exitCode), 0644)
+				g.Expect(err).To(BeNil())
+			}
+			validationMode := getValidationMode(exitCodeFilePath)
+			g.Expect(validationMode).To(Equal(entry.expectedValidationMode))
+			// Cleanup -> delete exit_code file
+			if entry.exitCode != "" {
+				err := os.Remove(exitCodeFilePath)
+				g.Expect(err).To(BeNil())
+			}
+		})
 	}
 }
--- internal/brclient/brclient_test.go
+++ internal/brclient/brclient_test.go
@@ -1,37 +1,62 @@
 package brclient
 
 import (
+	"bytes"
 	"context"
+	"github.com/gardener/etcd-wrapper/internal/types"
 	"io"
 	"net/http"
-	"net/http/httptest"
 	"os"
 	"testing"
-)
+	"time"
 
-var (
-	testClient *http.Client
-	testServer *httptest.Server
-	httpClient BackupRestoreClient
+	. "github.com/onsi/gomega"
 )
 
 var (
 	pwd, _             = os.Getwd()
 	etcdConfigFilePath = pwd + "/../../test/etcd-config.yaml"
+	caFilePath         = pwd + "/../../test/CAFile.crt"
+	// sample ca file taken from https://go.dev/src/crypto/x509/x509_test.go
+	ca = `
+-----BEGIN CERTIFICATE-----
+MIIDBjCCAe6gAwIBAgIRANXM5I3gjuqDfTp/PYrs+u8wDQYJKoZIhvcNAQELBQAw
+EjEQMA4GA1UEChMHQWNtZSBDbzAeFw0xODAzMjcxOTU2MjFaFw0xOTAzMjcxOTU2
+MjFaMBIxEDAOBgNVBAoTB0FjbWUgQ28wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAw
+ggEKAoIBAQDK+9m3rjsO2Djes6bIYQZ3eV29JF09ZrjOrEHLtaKrD6/acsoSoTsf
+cQr+rzzztdB5ijWXCS64zo/0OiqBeZUNZ67jVdToa9qW5UYe2H0Y+ZNdfA5GYMFD
+yk/l3/uBu3suTZPfXiW2TjEi27Q8ruNUIZ54DpTcs6y2rBRFzadPWwn/VQMlvRXM
+jrzl8Y08dgnYmaAHprxVzwMXcQ/Brol+v9GvjaH1DooHqkn8O178wsPQNhdtvN01
+IXL46cYdcUwWrE/GX5u+9DaSi+0KWxAPQ+NVD5qUI0CKl4714yGGh7feXMjJdHgl
+VG4QJZlJvC4FsURgCHJT6uHGIelnSwhbAgMBAAGjVzBVMA4GA1UdDwEB/wQEAwIF
+oDATBgNVHSUEDDAKBggrBgEFBQcDATAMBgNVHRMBAf8EAjAAMCAGA1UdEQQZMBeC
+FVRlc3RTeXN0ZW1DZXJ0UG9vbC5nbzANBgkqhkiG9w0BAQsFAAOCAQEAwuSRx/VR
+BKh2ICxZjL6jBwk/7UlU1XKbhQD96RqkidDNGEc6eLZ90Z5XXTurEsXqdm5jQYPs
+1cdcSW+fOSMl7MfW9e5tM66FaIPZl9rKZ1r7GkOfgn93xdLAWe8XHd19xRfDreub
+YC8DVqgLASOEYFupVSl76ktPfxkU5KCvmUf3P2PrRybk1qLGFytGxfyice2gHSNI
+gify3K/+H/7wCkyFW4xYvzl7WW4mXxoqPRPjQt1J423DhnnQ4G1P8V/vhUpXNXOq
+N9IEPnWuihC09cyx/WMQIUlWnaQLHdfpPS04Iez3yy2PdfXJzwfPrja7rNE+skK6
+pa/O1nF0AfWOpw==
+-----END CERTIFICATE-----
+	`
 )
 
-//func TestSuit(t *testing.T) {
-//	allTests := []struct {
-//		name   string
-//		testFn func(t *testing.T)
-//	}{
-//		{"getEtcdConfig", testGetEtcdConfig},
-//	}
-//
-//	for _, entry := range allTests {
-//		t.Run(entry.name, entry.testFn)
-//	}
-//}
+func TestSuit(t *testing.T) {
+	allTests := []struct {
+		name   string
+		testFn func(t *testing.T)
+	}{
+		{"getEtcdConfig", testGetEtcdConfig},
+		{"getInitializationStatus", testGetInitializationStatus},
+		{"triggerInitializer", testTriggerInitialization},
+		{"createSidecarClient", testCreateSidecarClient},
+		{"createTLSConfig", testCreateTLSConfig},
+	}
+
+	for _, entry := range allTests {
+		t.Run(entry.name, entry.testFn)
+	}
+}
 
 type TestRoundTripper func(req *http.Request) *http.Response
 
@@ -39,94 +64,179 @@ func (f TestRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	return f(req), nil
 }
 
-//func testGetEtcdConfig(t *testing.T) {
-//	table := []struct {
-//		description  string
-//		responseCode int
-//		responseBody []byte
-//		expectError  bool
-//	}{
-//		{"test: 200 response code should return a valid etcd config", http.StatusOK, []byte("give me a valid etcd config"), false},
-//	}
-//
-//	for _, entry := range table {
-//		t.Log(entry.description)
-//		httpClient := &http.Client{
-//			Transport: TestRoundTripper(func(req *http.Request) *http.Response {
-//				var contentLen int64
-//				if entry.responseBody != nil {
-//					contentLen = int64(len(entry.responseBody))
-//				}
-//				return &http.Response{
-//					StatusCode:    entry.responseCode,
-//					Body:          io.NopCloser(bytes.NewReader(entry.responseBody)),
-//					ContentLength: contentLen,
-//				}
-//			}),
-//			Timeout: 5 * time.Second,
-//		}
-//		NewClient(httpClient, )
-//	}
-//}
-
-func TestBrClient_TestGetEtcdConfig(t *testing.T) {
-	_, err := os.Getwd()
-	if err != nil {
-		t.Errorf("Error getting present working directory: %v", err)
-	}
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		_, _ = io.WriteString(w, "Sample etcd config")
-	}
-	testServer = httptest.NewServer(http.HandlerFunc(handler))
-	testClient = testServer.Client()
-	httpClient, err = NewClient(testClient, testServer.URL, etcdConfigFilePath)
-	if err != nil {
-		t.Errorf("Error creating test client: %v", err)
-	}
-	req, err := httpClient.GetEtcdConfig(context.TODO())
-	if err != nil {
-		t.Errorf("Error fetching etcd config: %v", err)
-	}
-	if req == "" {
-		t.Error("Invalid config file path returned")
-	}
-	testServer.Close()
-	err = os.Remove(etcdConfigFilePath)
-	if err != nil {
-		t.Errorf("Error removing etcd config file: %v", err)
-	}
-}
-
-func TestBrClient_GetInitializationStatus(t *testing.T) {
-	tests := []struct {
-		name                 string
-		description          string
-		serverReturnedStatus InitStatus
-		expectedStatus       InitStatus
-		run                  func(t *testing.T, serverReturnedStatus InitStatus, expectedStatus InitStatus)
+func testGetEtcdConfig(t *testing.T) {
+	table := []struct {
+		description  string
+		responseCode int
+		responseBody []byte
+		expectError  bool
 	}{
-		{"New", "Initialization status returned by server is New", New, New, testGetInitializationStatus},
-		{"InProgress", "Initialization status returned by server is InProgress", InProgress, InProgress, testGetInitializationStatus},
-		{"Successful", "Initialization status returned by server is Successful", Successful, Successful, testGetInitializationStatus},
-		{"Successful", "Initialization status returned by server is who knows", 4, InProgress, testGetInitializationStatus},
+		{"test: 200 response code should return a valid etcd config", http.StatusOK, []byte("give me a valid etcd config"), false},
+		{"test: 202 response code should return a valid etcd config", http.StatusAccepted, []byte("give me a valid etcd config"), false},
+		{"test: 201 response code should return a valid etcd config", http.StatusCreated, []byte("give me a valid etcd config"), false},
+		{"test: 208 response code should return an error", http.StatusAlreadyReported, []byte("give me a valid etcd config"), true},
+		{"test: 400 response code should return an error", http.StatusBadRequest, []byte("give me a valid etcd config"), true},
 	}
-	for _, test := range tests {
-		t.Run(test.description, func(t *testing.T) {
-			test.run(t, test.serverReturnedStatus, test.expectedStatus)
-		})
+
+	g := NewWithT(t)
+	defer deleteTestFiles(g)
+	for _, entry := range table {
+		t.Log(entry.description)
+		httpClient := getTestHttpClient(entry.responseCode, entry.responseBody)
+		brclient, err := NewClient(httpClient, "", etcdConfigFilePath)
+		g.Expect(err).To(BeNil())
+		req, err := brclient.GetEtcdConfig(context.TODO())
+		if entry.expectError {
+			g.Expect(err).ToNot(BeNil())
+			g.Expect(req).To(Equal(""))
+		} else {
+			g.Expect(err).To(BeNil())
+			g.Expect(req).To(Equal(etcdConfigFilePath))
+		}
 	}
 }
 
-func testGetInitializationStatus(t *testing.T, serverReturnedStatus InitStatus, expectedStatus InitStatus) {
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		_, _ = io.WriteString(w, serverReturnedStatus.String())
+func testGetInitializationStatus(t *testing.T) {
+	table := []struct {
+		description    string
+		responseCode   int
+		responseBody   []byte
+		expectError    bool
+		expectedStatus InitStatus
+	}{
+		{"test: `New` initialization status returned by server should result in `New`", http.StatusOK, []byte(New.String()), false, New},
+		{"test: `InProgress` initialization status returned by server should result in `InProgress`", http.StatusOK, []byte(InProgress.String()), false, InProgress},
+		{"test: `Successful` initialization status returned by server should result in  Successful", http.StatusOK, []byte(Successful.String()), false, Successful},
+		{"test: Unknown initialization status returned by server should result in `InProgress`", http.StatusOK, []byte("error response"), false, InProgress},
+		{"test: bad response from server should result in `Unknown`", http.StatusBadRequest, []byte("error response"), true, Unknown},
 	}
-	testServer = httptest.NewServer(http.HandlerFunc(handler))
-	testClient = testServer.Client()
-	httpClient, _ = NewClient(testClient, testServer.URL, DefaultEtcdConfigFilePath)
-	req, _ := httpClient.GetInitializationStatus(context.TODO())
-	if req != expectedStatus {
-		t.Errorf("Wrong status read %s. Expected %s, received %s", req.String(), expectedStatus.String(), serverReturnedStatus.String())
+
+	g := NewWithT(t)
+	defer deleteTestFiles(g)
+	for _, entry := range table {
+		t.Log(entry.description)
+		httpClient := getTestHttpClient(entry.responseCode, entry.responseBody)
+		brclient, err := NewClient(httpClient, "", etcdConfigFilePath)
+		g.Expect(err).To(BeNil())
+		req, err := brclient.GetInitializationStatus(context.TODO())
+		g.Expect(err != nil).To(Equal(entry.expectError))
+		g.Expect(req).To(Equal(entry.expectedStatus))
 	}
-	testServer.Close()
+}
+
+func testTriggerInitialization(t *testing.T) {
+	table := []struct {
+		description  string
+		responseCode int
+		responseBody []byte
+		expectError  bool
+	}{
+		{"test: server returning a valid response should not result in an error", http.StatusOK, []byte("valid server response"), false},
+		{"test: server returning an error code should result in an error", http.StatusBadRequest, []byte("invalid server response"), true},
+	}
+
+	for _, entry := range table {
+		t.Log(entry.description)
+		g := NewWithT(t)
+		httpClient := getTestHttpClient(entry.responseCode, entry.responseBody)
+		brclient, err := NewClient(httpClient, "", etcdConfigFilePath)
+		g.Expect(err).To(BeNil())
+		err = brclient.TriggerInitialization(context.TODO(), FullValidation)
+		g.Expect(err != nil).To(Equal(entry.expectError))
+	}
+}
+
+func TestNewDefaultClient(t *testing.T) {
+	incorrectCAFilePath := caFilePath + "/wrong-path"
+	table := []struct {
+		description   string
+		sidecarConfig types.SidecarConfig
+		expectError   bool
+	}{
+		{"test: return error when incorrect sidecar config is passed", types.SidecarConfig{TLSEnabled: true, CaCertBundlePath: &incorrectCAFilePath}, true},
+		{"test: return backuprestore client when valid sidecar config is passed", types.SidecarConfig{TLSEnabled: true, CaCertBundlePath: &caFilePath}, false},
+	}
+	g := NewWithT(t)
+	createTestCACertFiles(g)
+	defer deleteTestFiles(g)
+	for _, entry := range table {
+		t.Log(entry.description)
+		_, err := NewDefaultClient(entry.sidecarConfig, DefaultEtcdConfigFilePath)
+		g.Expect(err != nil).To(Equal(entry.expectError))
+	}
+}
+
+func testCreateSidecarClient(t *testing.T) {
+	incorrectCAFilePath := caFilePath + "/wrong-path"
+	table := []struct {
+		description   string
+		sidecarConfig types.SidecarConfig
+		expectError   bool
+	}{
+		{"test: return error when incorrect sidecar config is passed", types.SidecarConfig{TLSEnabled: true, CaCertBundlePath: &incorrectCAFilePath}, true},
+		{"test: return etcd client when valid sidecar config is passed", types.SidecarConfig{TLSEnabled: true, CaCertBundlePath: &caFilePath}, false},
+	}
+	g := NewWithT(t)
+	createTestCACertFiles(g)
+	defer deleteTestFiles(g)
+	for _, entry := range table {
+		t.Log(entry.description)
+		_, err := createSidecarClient(entry.sidecarConfig)
+		g.Expect(err != nil).To(Equal(entry.expectError))
+	}
+}
+
+func testCreateTLSConfig(t *testing.T) {
+	incorrectCAFilePath := caFilePath + "/wrong-path"
+	table := []struct {
+		description   string
+		sidecarConfig types.SidecarConfig
+		expectError   bool
+	}{
+		{"test: return valid insecure TLS config when sidecar config does not have TLS enabled", types.SidecarConfig{TLSEnabled: false}, false},
+		{"test: return valid TLS config when sidecar config has TLS enabled and valid CA file path", types.SidecarConfig{TLSEnabled: true, CaCertBundlePath: &caFilePath}, false},
+		{"test: return error when sidecar config has TLS enabled and invalid CA file path", types.SidecarConfig{TLSEnabled: true, CaCertBundlePath: &incorrectCAFilePath}, true},
+	}
+
+	g := NewWithT(t)
+	createTestCACertFiles(g)
+	defer deleteTestFiles(g)
+	for _, entry := range table {
+		t.Log(entry.description)
+		_, err := createTLSConfig(entry.sidecarConfig)
+		g.Expect(err != nil).To(Equal(entry.expectError))
+	}
+}
+
+func getTestHttpClient(responseCode int, responseBody []byte) *http.Client {
+	return &http.Client{
+		Transport: TestRoundTripper(func(req *http.Request) *http.Response {
+			var contentLen int64
+			if responseBody != nil {
+				contentLen = int64(len(responseBody))
+			}
+			return &http.Response{
+				StatusCode:    responseCode,
+				Body:          io.NopCloser(bytes.NewReader(responseBody)),
+				ContentLength: contentLen,
+			}
+		}),
+		Timeout: 5 * time.Second,
+	}
+}
+
+func createTestCACertFiles(g *GomegaWithT) {
+	err := os.WriteFile(caFilePath, []byte(ca), 0644)
+	g.Expect(err).To(BeNil())
+}
+
+func deleteTestFiles(g *GomegaWithT) {
+	if _, err := os.Stat(etcdConfigFilePath); err == nil {
+		err = os.Remove(etcdConfigFilePath)
+		g.Expect(err).To(BeNil())
+	}
+	if _, err := os.Stat(caFilePath); err == nil {
+		err = os.Remove(caFilePath)
+		g.Expect(err).To(BeNil())
+	}
 }
