28e2d84dd9d8218fffde7348829d5e44701e0fce
Madhav Bhargava <madhav.bhargava@sap.com>
2023-04-14 08:45:47 +0530

    addressed review comments from Sesha

--- internal/signal/signal.go
+++ internal/signal/signal.go
@@ -30,7 +30,7 @@ var (
 // Callback is a callback that will be invoked when one of the shutdownSignals
 // is caught. It is required that the implementation of this function should be very quick to ensure that it
 // finishes in time before the subsequent signal is caught which will result in a forced exit of the app.
-type Callback[T any] func(os.Signal, T)
+type Callback[T any] func(os.Signal, T) error
 
 // SetupHandler sets up a context which reacts to shutdownSignals
 func SetupHandler[T any](logger *zap.Logger, callback Callback[T], callbackParam T) (context.Context, context.CancelFunc) {
@@ -40,7 +40,10 @@ func SetupHandler[T any](logger *zap.Logger, callback Callback[T], callbackParam
 
 	go func() {
 		sig := <-notifierCh
-		callback(sig, callbackParam) //invoking the callback to process the signal
+		//invoking the callback to process the signal
+		if err := callback(sig, callbackParam); err != nil {
+			logger.Error("failed to capture exit code", zap.Error(err))
+		}
 		logger.Info("caught shutdown signal", zap.Any("signal", sig))
 		cancelFn()
 		<-notifierCh
