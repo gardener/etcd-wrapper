0cc576229e05a052757f7c457941b8657958dcb9
Seshachalam Yerasala Venkata <seshachalam.yerasala.venkata@sap.com>
2023-05-03 16:42:21 +0530

    Fixed CI script common.sh

--- .ci/common.sh
+++ .ci/common.sh
@@ -13,19 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# The `go <cmd>` commands requires to see the target repository to be part of a
+# Go workspace. Thus, if we are not yet in a Go workspace, let's create one
+# temporarily by using symbolic links.
+set -eo pipefail
 
-# For the test step concourse will set the following environment variables:
-# SOURCE_PATH - path to component repository root directory.
-if [[ $(uname) == 'Darwin' ]]; then
-  READLINK_BIN="greadlink"
-else
-  READLINK_BIN="readlink"
-fi
-
-if [[ -z "${SOURCE_PATH}" ]]; then
-  export SOURCE_PATH="$(${READLINK_BIN} -f "$(dirname ${0})/..")"
-else
-  export SOURCE_PATH="$(${READLINK_BIN} -f "${SOURCE_PATH}")"
+if [[ -z "$SOURCE_PATH" ]]; then
+    echo "Environment variable SOURCE_PATH must be provided"
+    exit 1
 fi
 
 VCS="github.com"
@@ -33,9 +28,6 @@ ORGANIZATION="gardener"
 PROJECT="etcd-wrapper"
 REPOSITORY=${VCS}/${ORGANIZATION}/${PROJECT}
 
-# The `go <cmd>` commands requires to see the target repository to be part of a
-# Go workspace. Thus, if we are not yet in a Go workspace, let's create one
-# temporarily by using symbolic links.
 if [[ "${SOURCE_PATH}" != *"src/${REPOSITORY}" ]]; then
   SOURCE_SYMLINK_PATH="${SOURCE_PATH}/tmp/src/${REPOSITORY}"
   if [[ -d "${SOURCE_PATH}/tmp" ]]; then
@@ -48,5 +40,21 @@ if [[ "${SOURCE_PATH}" != *"src/${REPOSITORY}" ]]; then
   export GOPATH="${SOURCE_PATH}/tmp"
   export GOBIN="${SOURCE_PATH}/tmp/bin"
   export PATH="${GOBIN}:${PATH}"
-  export GO111MODULE=on
 fi
+
+# Turn colors in this script off by setting the NO_COLOR variable in your
+# environment to any value:
+#
+# $ NO_COLOR=1 test.sh
+NO_COLOR=${NO_COLOR:-""}
+if [ -z "$NO_COLOR" ]; then
+  header=$'\e[1;33m'
+  reset=$'\e[0m'
+else
+  header=''
+  reset=''
+fi
+
+function header_text {
+  echo "$header$*$reset"
+}
