cf0e35b32934fdf054bd550082bd5d84737c0eda
Madhav Bhargava <madhav.bhargava@sap.com>
2023-05-17 09:38:40 +0530

    addressed review comments

--- internal/types/config.go
+++ internal/types/config.go
@@ -22,15 +22,6 @@ import (
 	"github.com/gardener/etcd-wrapper/internal/util"
 )
 
-const (
-	// DefaultTLSEnabled defines the default TLS state of the application
-	DefaultTLSEnabled = false
-	// DefaultSideCarHostPort defines the default sidecar host and port
-	DefaultSideCarHostPort = ":8080"
-	// DefaultExitCodeFilePath defines the default file path for the file that stores the exit code of the previous run
-	DefaultExitCodeFilePath = "/var/etcd/data/exit_code"
-)
-
 // SidecarConfig defines parameters needed for the sidecar
 type SidecarConfig struct {
 	HostPort         string
